fd0f348be234e2266f9c2b527a0652f35c56c098
Gleb <57363240+Molotovnikov@users.noreply.github.com>
2020-09-29 18:29:45 +0300

    Commit seven

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 import './scss/app.scss';
+let year = new Date().getFullYear();
 
 $(".burger-icon").on("click", function () {
     $(".header-nav").slideToggle();
     $(".burger-menu").toggleClass("burger-menu--opened")
 });
 
-$('.carousel-wrapper').slick({
+$('.first-carousel').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
     arrows: false
 });
+
+$('.carousel-wrapper-second').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+});
+
+$(".current-year").text(year);
+
+// ajax call to Products
+// $.ajax({
+//     url: './data.json',
+//     dataType: 'json',
+//     crossDomain: true,
+//     method: 'GET',
+//     success: function(data) {
+//         console.log(data)
+//     }
+// });
